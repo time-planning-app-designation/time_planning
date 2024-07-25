1d6bf49385df306d2e5c4b4f603bba20543df8a4
evanCai1164 <1164759627@qq.com>
2024-07-25 21:47:49 +0800

    添加结构

---
+++ src/main/java/com/example/time_planning/TimePlanningApplication.java
@@ -0,0 +1,13 @@
+package com.example.time_planning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimePlanningApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TimePlanningApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/time_planning/TimePlanningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.time_planning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimePlanningApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
